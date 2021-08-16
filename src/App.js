f4c688bb31b2e6d163c726493b51c207291f98ce
Nicklas Hansen <NicklasYoudendk@gmail.com>
2021-08-16 12:22:11 +0200

    notify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
 import SerchBar from './components/Searchbar';
 import SearchContext from "./context/SearchContext";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Results from './views/results';
 
 
 function App() {
   var searchState = useState([])
+  
+  useEffect(() => {
+    Notification.requestPermission(function(status) {
+      console.log('Notification permission status:', status);
+    });
+  });
+
+  function displayNotification() {
+    if (Notification.permission === 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        reg.showNotification('Hello world!',{
+          vibrate: [200, 100, 200, 500, 100, 300, 200, 400, 2000, 50]
+        });
+      });
+    }
+  }
 
   return (
     <SearchContext.Provider value={searchState}>
     <div className="App">
       <SerchBar/>
       <Results/>
+      <button onClick={displayNotification}>Notify me</button>
     </div>
     </SearchContext.Provider>
   );
